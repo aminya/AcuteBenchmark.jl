97feba93f3bb34d398854c94eabefc3d66823797
aminya <aminyahyaabadi74@gmail.com>
2019-12-23 02:15:18 +0330

    uniqueType for bardim

--- src/AcuteBenchmark.jl
+++ src/AcuteBenchmark.jl
@@ -269,18 +269,23 @@ It is assumed that number of dimension sets are the same.
 bardim(configs)
 ```
 """
-function bardim(config::StructArray{Funb})
+function bardim(config::StructArray{Funb}, uniqueType::Bool = false)
 
     bar_width = 0.2
     bar_text_font = Int64(bar_width*40)
     dim_text_font = Int64(bar_width*30)
     xticks_font = Int64(bar_width*5)
 
+    if uniqueType
+        uniqueTypes = uniqueflatten(config.types[:])
+        numUniqueTypes =length(uniqueTypes)
+        colors = distinguishable_colors(numUniqueTypes, [RGB(1,1,1)])
+    end
+
     numFun = length(config.fun)
     _, numDimsSets = numArgsDims(config.dims[1])
 
     for iDimSet = 1:numDimsSets
-
         plt = plot()  # different figure for different dims
 
         for iFun = 1:numFun
@@ -291,22 +296,32 @@ function bardim(config::StructArray{Funb})
 
                 x = [(iFun-1)+(bar_width*(iType-1))]
 
-                barText = Plots.text(string(config.types[iFun][iType]), pointsize = bar_text_font, :center, rotation = 90 )
+                if uniqueType
+                    iTypeUnique = findfirst(x -> x==config.types[iFun][iType], uniqueTypes)
+                    fillcolor = colors[iTypeUnique]
+                    type_text_color = invert(fillcolor, true)
+                else
+                    fillcolor = :auto
+                    type_text_color = :black
+                end
+
+                barText = Plots.text(string(config.types[iFun][iType]), pointsize = bar_text_font, :center, rotation = 90, color = type_text_color )
 
                 dimText = Plots.text(stringMatrix(config.dims[iFun][:,iDimSet]), pointsize = dim_text_font, :center)
 
 
-                    y = [config.median[iFun][iType][iDimSet]]
-                    # adding bar
-                    bar!(plt,
-                        x,
-                        y,
-                        # labels = string(config.types[iFun][iType][iDimSet])[1],
-                        legend = false,
-                        bar_width = bar_width,
-                        annotations = ([x, x], [y./2, y .+ bar_width/8], [barText, dimText]),
-                        dpi = 600
-                    )
+                y = [config.median[iFun][iType][iDimSet]]
+                # adding bar
+                bar!(plt,
+                    x,
+                    y,
+                    # labels = string(config.types[iFun][iType][iDimSet])[1],
+                    legend = false,
+                    fillcolor = fillcolor,
+                    bar_width = bar_width,
+                    annotations = ([x, x], [y./2, y .+ bar_width/8], [barText, dimText]),
+                    dpi = 600
+                )
             end
         end
 
