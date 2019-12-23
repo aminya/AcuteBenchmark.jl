badf4ec89155f263fb21c11cf94e44d03fa6965a
aminya <aminyahyaabadi74@gmail.com>
2019-12-23 18:50:29 +0330

    dimplot

--- src/plots.jl
+++ src/plots.jl
@@ -1,6 +1,6 @@
 using Plots, Colors, ColorTypes # for plotting
 
-export bar
+export bar, dimplot
 ################################################################
 
 flatten(A) =reduce(hcat, A)
@@ -32,6 +32,7 @@ To have a same color for the same types, set true as the 2nd argument
 # Examples
 ```julia
 bar(configs)
+bar(configs, true)
 ```
 """
 function bar(config::StructArray{Funb}, uniqueType::Bool = false)
@@ -113,6 +114,9 @@ Gets a pair of StructArrays and calculates relative speed of coresponding elemen
 Uses the first element of the pair for the configurations and only uses runtimes from the 2nd element of the pair. The relative speed is calculated like this:
 
 1st element runtimes / 2nd element runtimes
+```julia
+bar(configs => configs, true)
+```
 """
 function bar(config::Pair{StructArray{Funb,T1,T2,T3}, StructArray{Funb,T1,T2,T3}}, uniqueType::Bool = false) where {T1,T2,T3}
 
@@ -179,7 +183,59 @@ function bar(config::Pair{StructArray{Funb,T1,T2,T3}, StructArray{Funb,T1,T2,T3}
     end
 end
 
+################################################################
+"""
+    dimplot(config::StructArray{Funb}, fungroup::Vector{Vector{String}})
 
-        savefig("bench-dims-set$iDimSet.png")
+Shows runtime of a function for different dimension sets. Functions can be grouped together to be shown in a single plot.
+
+# Examples
+```julia
+configs2 = Funb( sin, [(-1,1)],[Float32, Float64], [10 20 30 40] );
+
+benchmark!(configs2)
+
+dimplot(configs2)
+```
+"""
+function dimplot(config::StructArray{Funb})
+
+bar_width = 0.2
+bar_text_font = Int64(bar_width*40)
+dim_text_font = Int64(bar_width*30)
+xticks_font = Int64(bar_width*5)
+
+numFun = length(config.fun)
+_, numDimsSets = numArgsDims(config.dims[1])
+
+    plt = plot()  # different figure for different dims
+
+    for iFun = 1:numFun
+
+        fname = [string(config.fun[iFun])]
+
+        for iType = 1:length(config.types[iFun])
+
+            x = 1:numDimsSets
+            xticks = string.(dropdims(flatten([config.dims[iFun][:,iDimSet] for iDimSet = 1:numDimsSets]), dims=1))
+            y = [config.median[iFun][iType][iDimSet] for iDimSet = 1:numDimsSets]
+            # adding bar
+            plot!(plt,
+                x,
+                y,
+                labels = string(config.types[iFun][iType]),
+                legend = :bottomright,
+                dpi = 600
+            )
+            xticks!(x, xticks, fontsize = xticks_font)
+        end
+
+        title!("$(fname[1])")
+        xlabel!("Dimension")
+        ylabel!("Time [micro seconds]")
+
+        savefig("bench-$(fname[1]).png")
     end
 end
+
+dimplot(config::Funb) = dimplot(FunbArray(config))
