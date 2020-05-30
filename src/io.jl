e0c9b135c5334c4a005228a1d50f486e0b0f37e2
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-05-30 18:13:20 -0500

    load and save function

--- src/io.jl
+++ src/io.jl
@@ -1,20 +1,49 @@
-import JLD2, FileIO # to save file
+using FileIO
 
 """
-    save(filename, configs)
+    save(file_name, config)
 
 Save benchmark data
 # Examples
 ```julia
-save("benchmarkdata.jld2", configs)
+AcuteBenchmark.save("benchmarkdata.jld2", config)
 ```
 """
-function save(filename::String, config::StructArray{Funb})
-    JLD2.@save(filename, config)
+function save(file_name::String, config::StructArray{Funb,T1,T2,T3}) where {T1,T2,T3}
+    file_name_parts = split(file_name, '.')
+    if length(file_name_parts) > 1 && file_name_parts[end] == "jld2"
+        # ignore
+    else
+        # to make sure it is saved with correct format
+        file_name = file_name * ".jld2"
+    end
+        
+    # convert to array before saving
+    configArray = Array(config)
+    
+    # Always save/load with configArray name
+    FileIO.save(file_name, Dict("configArray"=>configArray))
 end
 
-function load(filename::String, varname::String = "config")
-    FileIO.load(filename, varname)
+"""
+    load(file_name)
+
+load benchmark data
+# Examples
+```julia
+configs = AcuteBenchmark.load("benchmarkdata.jld2")
+```
+"""
+function load(file_name::String)
+    file_name_parts = split(file_name, '.')
+    if length(file_name_parts) > 1 && file_name_parts[end] == "jld2"
+        # ignore
+    else
+        # to make sure it is saved with correct format
+        file_name = file_name * ".jld2"
+    end
+    # Always save/load with configArray name
+    return FunbArray(FileIO.load(file_name)["configArray"])
 end
 
 ################################################################
